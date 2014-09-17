4dea6221951807dca88c946422893bb92e7fa4ce
termith <dmitdemidov@gmail.com>
2014-09-17 18:05:26 +0400

    refactoring

--- rectangle.py
+++ rectangle.py
@@ -1,25 +1,14 @@
 import sys
 
-if __name__ == '__main__':
-
-    if len(sys.argv) < 2:
-        input_file = "./input.txt"
-    else:
-        input_file = sys.argv[1]
-
-    x_array = []
-    y_array = []
-
-    print(type(x_array))
-
+def fill_arrays(filename):
     # Open input file
-    with open(input_file) as f:
+    with open(filename) as f:
         # For source rectangles
         rect_array = []
+        x_array = []
+        y_array = []
 
         # Coordinates arrays will represent a grid
-
-        # Fill arrays
         for line in f.readlines():
             x1, y1, x2, y2 = line[0:-1].split(" ")
             x_array.append(int(x1))
@@ -33,35 +22,46 @@ if __name__ == '__main__':
     x_array = list(set(sorted(x_array)))
     y_array = list(set(sorted(y_array)))
 
-    print(type(x_array))
+    return x_array, y_array, rect_array
 
 
-    def square(rectangle):
-        return (rectangle[2] - rectangle[0]) * (rectangle[3] - rectangle[1])  # (x2-x1)*(y2-y1)
+def square(rectangle):
+    return (rectangle[2] - rectangle[0]) * (rectangle[3] - rectangle[1])  # (x2-x1)*(y2-y1)
 
 
-    def check_rectangle(mesh):
-        for rectangle in rect_array:
-            if (mesh[2] >= rectangle[0] and mesh[2] <= rectangle[2]) and (
-                            mesh[3] >= rectangle[1] and mesh[3] <= rectangle[3]):
-                return True
-        return False
+def check_rectangle(mesh, rect_array):
+    for rectangle in rect_array:
+        if (mesh[2] >= rectangle[0] and mesh[2] <= rectangle[2]) and (
+                        mesh[3] >= rectangle[1] and mesh[3] <= rectangle[3]):
+            return True
+    return False
 
+def main():
+    input_file = sys.argv[1] if len(sys.argv) > 1 else './input.txt'
 
+    x_array, y_array, rect_array = fill_arrays(input_file)
+    
     result = 0
     # Go through x-array:
     for i in range(len(x_array)-1):
         for j in range(len(y_array)-1):
             mesh = [x_array[i], y_array[j], x_array[i + 1], y_array[j + 1]]
-            if check_rectangle(mesh):
+            if check_rectangle(mesh, rect_array):
                 result += square(mesh)
+    print(result)
 
-                # Go through y-array:
-                # Check if rectangle x1x2y1y2 is in target figure
-                # If it is:
-                # Result += square of rectangle
+if __name__ == '__main__':
+    main()
 
-print(result)
+
+
+
+
+
+
+
+
+
 
 
 
