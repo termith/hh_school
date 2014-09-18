13782a57215483f4f7a8396aa498c92093f641cd
ddemidov <ddemidov@2reallife.com>
2014-09-18 16:16:30 +0400

    small changes

--- substring.py
+++ substring.py
@@ -1,22 +1,17 @@
-import sys
-
 def search_std_lib(substring):
     last_number = 0
     sequence = ""
     while True:
         if sequence.find(substring) == -1:
-            sequence += str(last_number+1)
+            sequence = sequence[1:] + str(last_number+1)
             last_number += 1
         else:
             return sequence.find(substring)+1
-
 
 if __name__ == '__main__':
     print(search_std_lib("6789"))
     print(search_std_lib("101"))
 
-    if len(sys.argv) > 1:
-        for substring in sys.argv[1:]:
-            print(search_std_lib(substring))
+
 
 
